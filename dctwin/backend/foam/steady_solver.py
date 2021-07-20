c79a03d895df0d4273bd0c2d2e6334180962f197
Neng <xiad0001@ntu.edu.sg>
2021-07-20 17:46:02 +0800

    feat(solve): reuse mesh

--- dctwin/backend/foam/steady_solver.py
+++ dctwin/backend/foam/steady_solver.py
@@ -3,6 +3,7 @@ form.k.value = 1.5 * Math.pow(form.Tu.value/100,2) * Math.pow(form.u_freestream.
 form.epsilon.value = 0.09 * Math.pow(form.k.value,1.5) / form.Tu_L.value
 form.omega.value = form.epsilon.value / (0.09 * form.k.value)
 """
+from dctwin.cli.main import mesh
 import shutil
 import subprocess
 from logging import Logger
@@ -76,82 +77,6 @@ def parse_result(case: str):
     return results[-1]
 
 
-def run(
-    room: Room,
-    steady=True,
-    output: str = None,
-    delta_t: int = 1,
-    write_interval: int = 100,
-    end_time: int = 500,
-    process_num: int = 1,
-    dry_run: bool = False,
-):
-    if steady:
-        solver = "buoyantBoussinesqSimpleFoam"
-    else:
-        solver = "buoyantBoussinesqPimpleFoam"
-
-    if output is not None:
-        environ.CASE_DIR = Path(output).absolute()
-        logger.info(f"Copying data to {environ.CASE_DIR}")
-
-        shutil.copytree("case/0", f"{output}/0")
-        shutil.copytree("case/constant", f"{output}/constant")
-        shutil.copytree("case/system", f"{output}/system")
-        Path(environ.CASE_DIR, "case.foam").touch(exist_ok=True)
-
-    generate_control_dict(
-        room.probes,
-        steady=steady,
-        delta_t=delta_t,
-        write_interval=write_interval,
-        end_time=end_time,
-        process_num=process_num,
-    )
-    builder = Builder(room)
-    builder.run()
-    logger.info(f"Generate boundary in {environ.CASE_DIR}")
-
-    if process_num > 1:
-        command = [
-            "docker",
-            "run",
-            "--rm",
-            "-v",
-            f"{environ.CASE_DIR}:/data",
-            "-w",
-            "/data",
-            "openfoamplus/of_v1912_centos73",
-            "bash",
-            "-c",
-            '"source /opt/OpenFOAM/setImage_v1912.sh '
-            "&& decomposePar -force "
-            f'&& mpirun -np {process_num} --allow-run-as-root {solver} -parallel"',
-        ]
-    else:
-        command = [
-            "docker",
-            "run",
-            "--rm",
-            "-v",
-            f"{environ.CASE_DIR}:/data",
-            "-w",
-            "/data",
-            "openfoamplus/of_v1912_centos73",
-            "bash",
-            "-c",
-            "source /opt/OpenFOAM/setImage_v1912.sh",
-            "&&",
-            "echo",
-            "&&",
-            solver,
-        ]
-    if dry_run is False:
-        subprocess.run(command)
-    else:
-        return command
-
-
 class SteadySolverBackend(Backend):
     docker_image = "openfoamplus/of_v1912_centos73"
     solver = "buoyantBoussinesqSimpleFoam"
@@ -170,7 +95,17 @@ class SteadySolverBackend(Backend):
             )
         return command
 
-    def run(self, room: Room):
+    def run(self, room: Room, mesh_path=None, output_dir=None):
+        if output_dir is not None:
+            if mesh_path is None:
+                mesh_path = environ.CASE_DIR
+
+            environ.CASE_DIR = Path(output_dir).absolute()
+            shutil.copytree(f"{mesh_path}/0", f"{output_dir}/0")
+            shutil.copytree(f"{mesh_path}/constant", f"{output_dir}/constant")
+            shutil.copytree(f"{mesh_path}/system", f"{output_dir}/system")
+            Path(environ.CASE_DIR, "case.foam").touch(exist_ok=True)
+
         generate_control_dict(
             room.probes,
             steady=True,
