d716061284507548dfbb5c9a0fe75f37f49be07e
Wang Ruihang <ruihang001@e.ntu.edu.sg>
2022-11-03 19:50:58 +0800

    ✨ feat: add eplus backend

---
+++ dctwin/backends/eplus/core.py
@@ -0,0 +1,257 @@
+import shutil
+import time
+import socket
+import datetime
+import opyplus as op
+
+from pathlib import Path
+from typing import List, Tuple, Union, Optional
+import xml.etree.ElementTree as ET
+from loguru import logger
+from dctwin.utils import EPlusEnvConfig
+from dctwin.backends.eplus.eplus_logger import EPlusOutputFormatter
+from dctwin.backends.core import Backend
+from dctwin.backends.eplus.parser import IDFParser
+from dctwin.utils import config
+
+
+class EplusBackend(Backend):
+    """
+    A class to handle the communication with the EnergyPlus with BCVTB
+    :param proto_config: the configuration of the eplus model
+    :param host: The host to connect to (default: localhost)
+    :param network: The network to connect to (default: host)
+    """
+    docker_image = "ghcr.io/cap-dcwiz/energyplus-9-5-0:latest"
+    _version = 2
+    _cur_sim_time = 0.0
+    _msg_buf_size = 2048
+    _encoding = "ISO-8859-1"
+
+    def __init__(
+        self,
+        proto_config: EPlusEnvConfig,
+        host: Optional[str] = "localhost",
+        network: Optional[str] = "host",
+        *args,
+        **kwargs
+    ) -> None:
+        super().__init__(*args, **kwargs)
+        self._network = network
+        self._host = host
+        self._socket = None
+        self._proto_config = proto_config
+        self._set_up_socket()
+
+    @staticmethod
+    def _get_one_episode_len(idf_path: str) -> float:
+        epm = op.Epm.load(idf_path)
+        run_periods = epm.RunPeriod.one()
+        begin_year = 2013
+        begin_month = run_periods.begin_month
+        begin_day_of_month = run_periods.begin_day_of_month
+        end_year = 2013
+        end_month = run_periods.end_month
+        end_day_of_month = run_periods.end_day_of_month
+        start_time = datetime.datetime(begin_year, begin_month, begin_day_of_month, 0, 0, 0)
+        end_time = datetime.datetime(end_year, end_month, end_day_of_month, 23, 0, 0) + datetime.timedelta(0, 3600)
+        delta_sec = (end_time - start_time).total_seconds()
+        return delta_sec
+
+    def _set_up_socket(self) -> None:
+        """ Create a socket for communication with the BCVTB
+        """
+        self._socket = socket.socket()
+        self._socket.settimeout(10)
+        self._socket.bind(("0.0.0.0", 0))
+        self._socket.listen()
+        if self._host is None:
+            self._host = socket.gethostbyname(socket.gethostname())
+        self._port = self._socket.getsockname()[1]
+        logger.info(f"socket listening on 0.0.0.0:{self._port}")
+
+    @staticmethod
+    def _create_socket_cfg(host: str, port: int) -> None:
+        """ Create socket config file so that BCVTB can work with
+        """
+        top = ET.Element('BCVTB-client')
+        ipc = ET.SubElement(top, 'ipc')
+        _ = ET.SubElement(ipc, 'socket', {'port': str(port), 'hostname': host})
+        tree = ET.ElementTree(top)
+        ET.indent(tree, space="\t", level=0)
+        with open(config.CASE_DIR.joinpath("socket.cfg"), 'wb') as f:
+            f.write('<?xml version="1.0" encoding="ISO-8859-1"?>\n'.encode("ISO-8859-1"))
+            tree.write(f)
+
+    def run(self, episode_idx: int = 0) -> Tuple[Union[float, None], Union[float, None]]:
+        self._pre_process(episode_idx)
+        obs, done = self._run_backend()
+        return obs, done
+
+    @property
+    def command(self) -> List[str]:
+        assert isinstance(config.eplus.weather_file, Path)
+        assert isinstance(config.eplus.idf_file, Path)
+        return [
+            f"/bin/bash",
+            f"-c",
+            f"/usr/local/EnergyPlus-9-5-0/energyplus "
+            f"-w {config.eplus.weather_file.name} "
+            f"-r {config.eplus.idf_file.name}"
+        ]
+    
+    def _parse_idf_and_gen_bcvtb_config(
+        self,
+        idf_path: Union[str, Path],
+    ) -> None:
+        logger.info("Parsing IDF file...")
+        self.idf_parser = IDFParser(idf_path=str(idf_path))
+        action_configs = self._proto_config.actions
+        observation_configs = self._proto_config.observations
+        self.idf_parser.batch_set_actions(action_configs)
+        self.idf_parser.batch_set_observations(observation_configs)
+        self.idf_parser.set_simulation_time(self._proto_config.simulation_time_config)
+        self.idf_parser.set_external_interface()
+        schedule_configs = self.idf_parser.batch_set_return_temperature_schedule(
+            env_config=self._proto_config,
+        )
+        self.idf_parser.save(save_path=str(idf_path))
+        logger.info("Generating BCVTB Config ...")
+        self.idf_parser.save_cfg_xml(
+            observation_configs=self._proto_config.observations,
+            action_configs=self._proto_config.actions,
+            save_path=config.CASE_DIR.joinpath("variables.cfg"),
+            schedule_configs=schedule_configs,
+        )
+
+    def _pre_process(self, episode_idx: int = 0) -> None:
+        # create case folder
+        config.CASE_DIR = Path(config.LOG_DIR).joinpath(
+            f"eplus_output/episode_{episode_idx}"
+        )
+        Path(config.CASE_DIR).mkdir(parents=True, exist_ok=True)
+        # copy idf and weather files to the CASE folder
+        if config.eplus.idf_file.exists() and config.eplus.weather_file.exists():
+            weather_path = Path(config.CASE_DIR).joinpath(
+                config.eplus.weather_file.name
+            )
+            idf_path = Path(config.CASE_DIR).joinpath(
+                config.eplus.idf_file.name
+            )
+            shutil.copy(config.eplus.idf_file, idf_path)
+            shutil.copy(config.eplus.weather_file, weather_path)
+        else:
+            raise FileNotFoundError(
+                "Please check if the idf file and weather file exist"
+            )
+        self._parse_idf_and_gen_bcvtb_config(
+            idf_path=str(idf_path),
+        )
+        self._end_sim_time = self._get_one_episode_len(str(idf_path))
+        self._create_socket_cfg(self._host, self._port)
+
+    @staticmethod
+    def _post_process() -> None:
+        """process the output of the energyplus simulation"""
+        time.sleep(10)
+        try:
+            EPlusOutputFormatter.group_into_csv(str(config.CASE_DIR))
+        except FileNotFoundError:
+            logger.exception(f"Failed to clean output dir {config.CASE_DIR}")
+
+    def _run_backend(self) -> Tuple[Union[float, None], Union[float, None]]:
+        self.run_container(
+            environment={
+                "BCVTB_HOME": "/usr/local/bcvtb",
+            },
+            background=True,
+            network=self._network,
+        )
+        try:
+            self._conn, addr = self._socket.accept()
+            logger.info(f"Got connection from {addr}")
+        except Exception as e:
+            logger.error(f"Failed to get message from EnergyPlus process: {str(e)}")
+            exit(-1)  # die, not need to handle it as there is no workarounds
+        return self.receive_status()  # as it cannot be done on the very first step
+
+    def _serialize(self, actions: list) -> str:
+        """Serialize actions into formatted string"""
+        flag = 0 if len(actions) else 1
+        meta_info = [
+            str(self._version),
+            str(flag),
+            str(len(actions)),
+            "0",
+            "0",
+            "{:20.15e}".format(self._cur_sim_time),
+        ]
+        action_str = []
+        for action in actions:
+            action_str.append("{:20.15e}".format(action))
+        ret = " ".join(meta_info + action_str)
+        ret += "\n"
+        return ret
+
+    def _deserialize(self, msg: str) -> Tuple[List[float], bool]:
+        """Deserialize received message from Eplus into list of raw observations"""
+        msg = msg.split(" ")
+        self._version = int(msg[0])
+        if int(msg[1]) == 1:  # no more obs, end
+            return [], True
+        try:
+            self._cur_sim_time = float(msg[5])
+        except IndexError:
+            logger.critical(f"Error Message: {msg}")
+            exit(-1)
+        observations = []
+        for i in range(6, len(msg)):
+            observations.append(float(msg[i]))
+        return observations, False
+
+    def send_action(self, action) -> None:
+        """
+        Send actions to Eplus
+        """
+        msg = self._serialize(action)
+        self._conn.send(msg.encode())
+
+    def _get_parsed_msg(self) -> Tuple[List[float], bool]:
+        return self._deserialize(
+            self._conn.recv(self._msg_buf_size).decode(encoding=self._encoding).strip()
+        )
+
+    def receive_status(self) -> Tuple[Union[List[float], None], bool]:
+        """
+        Receive observations from Eplus
+        """
+        obs, terminated = self._get_parsed_msg()
+        if terminated:
+            logger.critical(
+                "Eplus terminated unexpectedly. "
+                "Likely to be the wrongly estimated simulation time. "
+                "Please check env._curSimTim"
+            )
+            exit(-1)
+        if self._cur_sim_time >= self._end_sim_time:
+            logger.debug("Came to the end of one episode, terminating")
+            done = True
+            self.close()
+        else:
+            done = False
+        return obs, done
+
+    def close(self) -> None:
+        self.send_action([])
+        self.container.stop()
+        self._post_process()
+
+    def __del__(self) -> None:
+        """Close Eplus process and socket connection
+        """
+        if hasattr(self, "_socket") and self._socket is not None:
+            logger.debug("Closing socket...")
+            if hasattr(self, "_conn"):
+                self._conn.close()
+            self._socket.close()
+        logger.debug("EnergyPlus backend closed")
---
+++ dctwin/backends/eplus/eplus_logger.py
@@ -0,0 +1,63 @@
+import gzip
+import os
+import shutil
+from loguru import logger
+
+
+class EPlusOutputFormatter:
+    ignored = [
+        # These are the messages counted as Severe, but actually ok (desired)
+        '   ** Severe  ** ExternalInterface: Received end of simulation flag'
+    ]
+
+    @classmethod
+    def group_into_csv(cls, episode_dir: str):
+        file_csv = episode_dir + '/eplusout.csv'
+        file_csv_gz = episode_dir + '/eplusout.csv.gz'
+        file_err = episode_dir + '/eplusout.err'
+        # files_to_preserve ['eplusout.csv', 'eplusout.err', 'eplustbl.htm']
+        files_to_clean = ['eplusmtr.csv', 'eplusout.audit', 'eplusout.bnd',
+                          'eplusout.dxf', 'eplusout.eio', 'eplusout.edd',
+                          'eplusout.end', 'eplusout.eso', 'eplusout.mdd',
+                          'eplusout.mtd', 'eplusout.mtr', 'eplusout.rdd',
+                          'eplusout.rvaudit', 'eplusout.shd', 'eplusssz.csv',
+                          'epluszsz.csv', 'sqlite.err']
+
+        # Check for any severe error
+        # num_errors = cls._count_severe_errors(file_err)
+        # if num_errors != 0:
+        #     logger.warning('EnergyPlusEnv: Severe error(s) occurred. Error count: {}'.format(num_errors))
+        #     logger.warning('EnergyPlusEnv: Check contents of {}'.format(file_err))
+            # sys.exit(1)
+
+        # Compress csv file and remove unnecessary files
+        # If csv file is not present in some reason, preserve all other files for inspection
+        if os.path.isfile(file_csv):
+            with open(file_csv, 'rb') as f_in:
+                with gzip.open(file_csv_gz, 'wb') as f_out:
+                    shutil.copyfileobj(f_in, f_out)
+            os.remove(file_csv)
+
+            for file in files_to_clean:
+                file_path = episode_dir + '/' + file
+                if os.path.isfile(file_path):
+                    os.remove(file_path)
+
+    @classmethod
+    def _count_severe_errors(cls, file):
+
+        if not os.path.isfile(file):
+            return -1  # Error count is unknown
+        fd = open(file)
+        lines = fd.readlines()
+        fd.close()
+        num_ignored = 0
+        for line in lines:
+            if line.find('************* EnergyPlus Completed Successfully') >= 0:
+                tokens = line.split()
+                return int(tokens[6]) - num_ignored
+            for pattern in cls.ignored:
+                if pattern in line:
+                    num_ignored += 1
+                    break
+        return -1
