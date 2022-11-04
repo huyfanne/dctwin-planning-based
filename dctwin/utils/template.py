8b5cd1da7120214cab972ee1f13bd0be0c6ef641
Wang Ruihang <ruihang001@e.ntu.edu.sg>
2022-11-04 19:10:51 +0800

    🚑 quick fix: disable autoescape

--- dctwin/utils/template.py
+++ dctwin/utils/template.py
@@ -4,5 +4,5 @@ from jinja2 import Environment, FileSystemLoader
 
 template_dir = Path(Path(__file__).parent.parent, "templates")
 template_env = Environment(
-    loader=FileSystemLoader(template_dir), trim_blocks=True, lstrip_blocks=True, autoescape=True,
+    loader=FileSystemLoader(template_dir), trim_blocks=True, lstrip_blocks=True,
 )
