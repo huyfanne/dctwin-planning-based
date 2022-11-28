479944cf2bd0b77380eadbf1191c1ffe460974cd
ruihangwang <ruihang001@e.ntu.edu.sg>
2022-11-28 10:32:05 +0800

    feat: enhance parser methods

--- dctwin/backends/foam/parser.py
+++ dctwin/backends/foam/parser.py
@@ -1,6 +1,9 @@
-from typing import Dict
+import numpy as np
+from typing import Dict, List
 from pathlib import Path
 from dctwin.models import Room
+
+from loguru import logger
 
 
 class RoomParser:
@@ -15,12 +18,39 @@ class RoomParser:
         room_path: Path = Path(""),
         room: Room = None
     ) -> None:
-        if room_path != Path("") and room_path.exists():
+        # boundary conditions
+        self._crac_setpoints_dict = {}
+        self._crac_volume_flow_rates_dict = {}
+        self._server_load_dict = {}
+        self._server_flow_rate_dict = {}
+        # sensor temperature
+        self._sensor_temp_dict = {}
+        # server type model
+        self._server_type_dict = {}
+        self._server_type_list = []
+        self._load_room_model(room_path, room)
+        self._load_server_type()
+
+    def _load_room_model(
+        self,
+        room_path: Path = Path(""),
+        room: Room = None,
+    ) -> None:
+        if Path(room_path) != Path("") and Path(room_path).absolute().exists():
             self.model = Room.load(file_path=room_path)
         elif room is not None:
             self.model = room
         else:
             raise ValueError("Either room_path or room must be provided.")
+
+    def _load_server_type(self) -> None:
+        for key, prop in self.model.objects.servers.items():
+            if prop.model is not None:
+                self._server_type_dict[prop.id] = prop.model
+                if prop.model not in self._server_type_list:
+                    self._server_type_list.append(prop.model)
+            else:
+                logger.warning(f"server {prop.id} type is not defined")
 
     @property
     def num_crac(self) -> int:
@@ -34,37 +64,97 @@ class RoomParser:
     def num_sen(self) -> int:
         return len(self.model.objects.sensors.items())
 
+    @property
+    def crac_list(self) -> list:
+        return list(self.model.objects.acus.keys())
+
+    @property
+    def ser_list(self) -> List:
+        return list(self.model.objects.servers.keys())
+
+    @property
+    def rack_list(self) -> List:
+        return list(self.model.objects.racks.keys())
+
+    @property
+    def sen_list(self) -> List:
+        return list(self.model.objects.sensors.keys())
+
+    @property
+    def server_type_list(self) -> List:
+        return self._server_type_list
+
+    @property
+    def server_type_dict(self) -> Dict:
+        return self._server_type_dict
+
+    @property
+    def acu_temp_array(self) -> np.ndarray:
+        return np.asarray(list(self._crac_setpoints_dict.values()))
+
+    @property
+    def acu_flow_array(self) -> np.ndarray:
+        return np.asarray(list(self._crac_volume_flow_rates_dict.values()))
+
+    @property
+    def ser_load_array(self) -> np.ndarray:
+        return np.asarray(list(self._server_load_dict.values()))
+
     def _update_crac_boundary(
         self,
         crac_setpoints: Dict,
-        crac_flow_rates: Dict
+        crac_volume_flow_rates: Dict,
     ) -> None:
         for uid, prop in self.model.objects.acus.items():
-            prop.supply_temperature = crac_setpoints[uid]
-            prop.flow_rate = crac_flow_rates[uid]
+            try:
+                prop.supply_temperature = crac_setpoints[uid]
+                self._crac_setpoints_dict[uid] = crac_setpoints[uid]
+            except KeyError:
+                logger.warning(f"CRAC {uid} setpoint is missing, set to 0")
+                prop.supply_temperature = 0
+                self._crac_setpoints_dict[uid] = 0 # that means the CRAC is off
+            try:
+                prop.flow_rate = crac_volume_flow_rates[uid]
+                self._crac_volume_flow_rates_dict[uid] = crac_volume_flow_rates[uid]
+            except KeyError:
+                logger.warning(f"CRAC {uid} volume flow rate is missing, set to 0")
+                prop.flow_rate = 0
+                self._crac_volume_flow_rates_dict[uid] = 0 # avoid zero division error
 
     def _update_server_boundary(
         self,
         server_powers: Dict,
-        server_flow_rates: Dict
+        server_volume_flow_rates: Dict,
     ) -> None:
         for uid, prop in self.model.objects.servers.items():
-            prop.heat_load = server_powers[uid]
-            if server_flow_rates:
-                prop.flow_rate = server_flow_rates[uid]
+            try:
+                prop.heat_load = server_powers[uid]
+                self._server_load_dict[uid] = server_powers[uid]
+            except KeyError:
+                logger.warning(f"server {uid} power is missing, set to 0")
+                prop.heat_load = 0
+                self._server_load_dict[uid] = 0
+            if server_volume_flow_rates:
+                try:
+                    prop.flow_rate = server_volume_flow_rates[uid]
+                    self._server_flow_rate_dict[uid] = server_volume_flow_rates[uid]
+                except KeyError:
+                    logger.warning(f"server {uid} volume flow rate is missing, set to 0")
+                    prop.flow_rate = 1e-6
+                    self._server_flow_rate_dict[uid] = 1e-6 # avoid zero division error
 
     def update_boundary_conditions(
         self,
         crac_setpoints: Dict,
         crac_volume_flow_rates: Dict,
         server_powers: Dict,
-        server_volume_flow_rates: Dict
+        server_volume_flow_rates: Dict,
     ) -> None:
         self._update_crac_boundary(crac_setpoints, crac_volume_flow_rates)
         self._update_server_boundary(server_powers, server_volume_flow_rates)
 
     @property
-    def format_boundary_conditions(self):
+    def format_boundary_conditions(self) -> Dict:
         boundary_conditions = {
             "crac_setpoints": {}, "crac_volume_flow_rates": {},
             "server_powers": {}, "server_volume_flow_rates": {}
