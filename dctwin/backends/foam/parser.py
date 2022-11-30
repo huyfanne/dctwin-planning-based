62672b45cac8cbdfa1cd9034ab4dc9bdbb3195cb
Wang Ruihang <ruihang001@e.ntu.edu.sg>
2022-11-30 16:40:06 +0800

    feat: add distance calculating methods

--- dctwin/backends/foam/parser.py
+++ dctwin/backends/foam/parser.py
@@ -1,7 +1,8 @@
+import math
 import numpy as np
 from typing import Dict, List
 from pathlib import Path
-from dctwin.models import Room
+from dctwin.models import Room, Vertex
 
 from loguru import logger
 
@@ -16,7 +17,7 @@ class RoomParser:
     def __init__(
         self,
         room_path: Path = Path(""),
-        room: Room = None
+        room: Room = None,
     ) -> None:
         # boundary conditions
         self._crac_setpoints_dict = {}
@@ -100,26 +101,62 @@ class RoomParser:
     def ser_load_array(self) -> np.ndarray:
         return np.asarray(list(self._server_load_dict.values()))
 
+    @property
+    def acu2sen(self) -> np.ndarray:
+        """ Get the distance matrix of the ACU to sensor connections
+        """
+        acu2sen_dis = np.zeros([self.num_crac, self.num_sen])
+        for acu_idx, acu in enumerate(self.model.objects.acus.values()):
+            for sen_idx, sen_loc in enumerate(self.model.probes):
+                acu_loc = acu.placement
+                acu2sen_dis[acu_idx][sen_idx] = self.euclidean_distance(acu_loc, sen_loc)
+        return acu2sen_dis
+
+    @property
+    def ser2sen(self) -> np.ndarray:
+        """ Get the distance matrix of the server to sensor connections
+        """
+        ser2sen_dis = np.zeros([self.num_ser, self.num_sen])
+        for ser_idx, ser in enumerate(self.model.objects.servers.values()):
+            for sen_idx, sen_loc in enumerate(self.model.probes):
+                ser_loc_i, ser_loc_o = self.model.server_patch_positions(ser.id)
+                ser2sen_dis[ser_idx][sen_idx] = self.euclidean_distance(ser_loc_o, sen_loc)
+        return ser2sen_dis
+
+    @property
+    def acu2ser(self) -> np.ndarray:
+        """ Get the distance matrix of the acu to server connections
+        """
+        acu2ser_dis = np.zeros([self.num_crac, self.num_ser])
+        for acu_idx, acu in enumerate(self.model.objects.acus.values()):
+            for ser_idx, ser in enumerate(self.model.objects.servers.values()):
+                acu_loc = acu.placement
+                ser_loc, _ = self.model.server_patch_positions(ser.id)
+                acu2ser_dis[acu_idx][ser_idx] = self.euclidean_distance(acu_loc, ser_loc)
+        return acu2ser_dis
+
+    @staticmethod
+    def euclidean_distance(loc_1: Vertex, loc_2: Vertex) -> float:
+        return math.sqrt((loc_1.x - loc_2.x) ** 2 + (loc_1.y - loc_2.y) ** 2 + (loc_1.z - loc_2.z) ** 2)
+
     def _update_crac_boundary(
         self,
         crac_setpoints: Dict,
         crac_volume_flow_rates: Dict,
     ) -> None:
         for uid, prop in self.model.objects.acus.items():
-            try:
-                prop.supply_temperature = crac_setpoints[uid]
-                self._crac_setpoints_dict[uid] = crac_setpoints[uid]
-            except KeyError:
-                logger.warning(f"CRAC {uid} setpoint is missing, set to 0")
-                prop.supply_temperature = 0
-                self._crac_setpoints_dict[uid] = 0 # that means the CRAC is off
-            try:
-                prop.flow_rate = crac_volume_flow_rates[uid]
-                self._crac_volume_flow_rates_dict[uid] = crac_volume_flow_rates[uid]
-            except KeyError:
-                logger.warning(f"CRAC {uid} volume flow rate is missing, set to 0")
-                prop.flow_rate = 0
-                self._crac_volume_flow_rates_dict[uid] = 0 # avoid zero division error
+            if crac_setpoints is not None:
+                try:
+                    prop.supply_temperature = crac_setpoints[uid]
+                    self._crac_setpoints_dict[uid] = crac_setpoints[uid]
+                except KeyError:
+                    logger.critical(f"CRAC {uid} setpoint is missing")
+            if crac_volume_flow_rates is not None:
+                try:
+                    prop.flow_rate = crac_volume_flow_rates[uid]
+                    self._crac_volume_flow_rates_dict[uid] = crac_volume_flow_rates[uid]
+                except KeyError:
+                    logger.critical(f"CRAC {uid} volume flow rate is missing")
 
     def _update_server_boundary(
         self,
@@ -127,28 +164,25 @@ class RoomParser:
         server_volume_flow_rates: Dict,
     ) -> None:
         for uid, prop in self.model.objects.servers.items():
-            try:
-                prop.heat_load = server_powers[uid]
-                self._server_load_dict[uid] = server_powers[uid]
-            except KeyError:
-                logger.warning(f"server {uid} power is missing, set to 0")
-                prop.heat_load = 0
-                self._server_load_dict[uid] = 0
-            if server_volume_flow_rates:
+            if server_powers is not None:
+                try:
+                    prop.heat_load = server_powers[uid]
+                    self._server_load_dict[uid] = server_powers[uid]
+                except KeyError:
+                    logger.critical(f"server {uid} power is missing")
+            if server_volume_flow_rates is not None:
                 try:
                     prop.flow_rate = server_volume_flow_rates[uid]
                     self._server_flow_rate_dict[uid] = server_volume_flow_rates[uid]
                 except KeyError:
-                    logger.warning(f"server {uid} volume flow rate is missing, set to 0")
-                    prop.flow_rate = 1e-6
-                    self._server_flow_rate_dict[uid] = 1e-6 # avoid zero division error
+                    logger.critical(f"server {uid} volume flow rate is missing")
 
     def update_boundary_conditions(
         self,
-        crac_setpoints: Dict,
-        crac_volume_flow_rates: Dict,
-        server_powers: Dict,
-        server_volume_flow_rates: Dict,
+        crac_setpoints: Dict = None,
+        crac_volume_flow_rates: Dict = None,
+        server_powers: Dict = None,
+        server_volume_flow_rates: Dict = None,
     ) -> None:
         self._update_crac_boundary(crac_setpoints, crac_volume_flow_rates)
         self._update_server_boundary(server_powers, server_volume_flow_rates)
