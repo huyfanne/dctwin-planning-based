0c2985d3c53c9fb7fea92945e6426916f4aca28b
Wang Ruihang <ruihang001@e.ntu.edu.sg>
2023-06-10 15:52:14 +0800

    bump up version to 0.6.15

--- dctwin/__init__.py
+++ dctwin/__init__.py
@@ -1,7 +1,7 @@
 from .registraion import make_env
 from dctwin import adapters, backends, interfaces, models, templates, utils
 
-__version__ = "0.6.9"
+__version__ = "0.6.15"
 
 __all__ = [
     "adapters",
