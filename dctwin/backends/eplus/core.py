443a2c86806b40948d6087c6978072e855aaf190
ruihangwang <ruihang001@e.ntu.edu.sg>
2023-06-14 18:57:04 +0800

    set keep-alive for socket

--- dctwin/backends/eplus/core.py
+++ dctwin/backends/eplus/core.py
@@ -68,6 +68,8 @@ class EplusBackend(Backend):
         """ Create a socket for communication with the BCVTB
         """
         self._socket = socket.socket()
+        # Enable keep-alive for the socket
+        self._socket.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
         self._socket.settimeout(None)
         self._socket.bind(("0.0.0.0", 0))
         self._socket.listen()
