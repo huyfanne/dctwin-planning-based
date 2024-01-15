fd46c10c836e0f72514d50e8a4d62a9c3d190b5e
nut3219 <wong1904@hotmail.com>
2024-01-15 11:05:29 +0800

    add sh

--- dctwin/__init__.py
+++ dctwin/__init__.py
@@ -1,7 +1,7 @@
 from .registraion import make_env
 from dctwin import adapters, backends, interfaces, models, templates, utils
 
-__version__ = "0.7.3"
+__version__ = "0.7.4"
 
 __all__ = [
     "adapters",
