a8297e3dff350ab8f208c792ea259f216e8cb7c4
Lee Wei Ming <leeweiming3@gmail.com>
2024-05-27 17:58:49 +0800

    Fix high vulns (change shell commands and escape html)

--- dctwin/utils/template.py
+++ dctwin/utils/template.py
@@ -5,6 +5,7 @@ from jinja2 import Environment, FileSystemLoader
 template_dir = Path(Path(__file__).parent.parent, "templates")
 template_env = Environment(
     loader=FileSystemLoader(template_dir),
+    autoescape=True,
     trim_blocks=True,
     lstrip_blocks=True,
 )
