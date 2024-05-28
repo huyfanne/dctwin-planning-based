1f90c2ebdb92bfdd9ce53860c3fafe0f9963ec8d
Lee Wei Ming <leeweiming3@gmail.com>
2024-05-28 11:28:13 +0800

    Fix autoescape in jinja2

--- dctwin/utils/template.py
+++ dctwin/utils/template.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 template_dir = Path(Path(__file__).parent.parent, "templates")
 template_env = Environment(
     loader=FileSystemLoader(template_dir),
-    autoescape=True,
+    autoescape=select_autoescape(
+            default_for_string=False,
+            default=False,
+        ),
     trim_blocks=True,
     lstrip_blocks=True,
 )
