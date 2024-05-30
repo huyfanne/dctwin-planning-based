e08ecce140f37010b6d1d3c296662289b5c06880
Lee Wei Ming <leeweiming3@gmail.com>
2024-05-30 15:14:21 +0800

    CI: streamline bandit and safety, & format

--- dctwin/utils/template.py
+++ dctwin/utils/template.py
@@ -6,9 +6,9 @@ template_dir = Path(Path(__file__).parent.parent, "templates")
 template_env = Environment(
     loader=FileSystemLoader(template_dir),
     autoescape=select_autoescape(
-            default_for_string=False,
-            default=False,
-        ),
+        default_for_string=False,
+        default=False,
+    ),
     trim_blocks=True,
     lstrip_blocks=True,
 )
