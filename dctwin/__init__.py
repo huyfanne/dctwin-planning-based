9f8763035fb151a5ca3174ce24ae2f7a818834b0
Koh Jun Jie <junjie.koh@rda.ai>
2024-06-07 09:54:00 +0800

    updated version to v1.4.4

--- dctwin/__init__.py
+++ dctwin/__init__.py
@@ -2,7 +2,7 @@ from .registraion import make_env
 from dctwin import adapters, backends, interfaces, templates, utils
 from dctwin.backends import IDFBuilder, ConfigBuilder
 
-__version__ = "1.4.3"
+__version__ = "1.4.4"
 
 __all__ = [
     "adapters",
