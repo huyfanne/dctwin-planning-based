5e6e871cd1432640a792018a132cfb038a390a00
Koh Jun Jie <junjie.koh@rda.ai>
2024-06-20 16:18:20 +0800

    added new models for testing

--- test/test_cfd.py
+++ test/test_cfd.py
@@ -7,6 +7,6 @@ config.PRESERVE_FOAM_LOG = True
 
 iterations = 50
 
-# manager = CFDManager(room=room, mesh_process=2, solve_process=2, is_gpu=True)
+# manager = CFDManager(room=room, mesh_process=4, solve_process=4, is_gpu=True)
 manager = CFDManager(room=room, mesh_process=2, solve_process=2, is_gpu=False)
 manager.run()
