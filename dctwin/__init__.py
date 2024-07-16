06687ddc481a0d8457ab19b37d357ba3e2491657
Koh Jun Jie <junjie.koh@rda.ai>
2024-07-16 12:21:30 +0800

    added configobj package and updated to v1.6.5

--- dctwin/__init__.py
+++ dctwin/__init__.py
@@ -2,7 +2,7 @@ from .registraion import make_env
 from dctwin import adapters, backends, interfaces, templates, utils
 from dctwin.backends import IDFBuilder, ConfigBuilder
 
-__version__ = "1.6.4"
+__version__ = "1.6.5"
 
 __all__ = [
     "adapters",
