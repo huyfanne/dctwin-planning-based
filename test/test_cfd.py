19964779d157df318831780cc1c5bf02ab9ad1ab
Koh Jun Jie <junjie.koh@rda.ai>
2024-07-25 10:50:53 +0800

    changed back to the default room model

--- test/test_cfd.py
+++ test/test_cfd.py
@@ -2,11 +2,12 @@ from dclib import Room
 from dctwin.interfaces import CFDManager
 from dctwin.utils import config
 
-room = Room.load("models/geometry/room_test.json")
+room = Room.load("/home/azureuser/dctwin/test/models/geometry/room_test.json")
 config.PRESERVE_FOAM_LOG = True
 
 iterations = 50
 
 # manager = CFDManager(room=room, mesh_process=4, solve_process=4, is_gpu=True)
-manager = CFDManager(room=room, mesh_process=2, solve_process=2, is_gpu=False)
+# manager = CFDManager(room=room, mesh_process=2, solve_process=2, is_gpu=False)
+manager = CFDManager(room=room, mesh_process=8, solve_process=8, is_gpu=False)
 manager.run()
