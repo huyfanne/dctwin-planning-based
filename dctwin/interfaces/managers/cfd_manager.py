6c6f6432fd0ad47cd03cf1b2762476c998ed79a8
Ng Jia Hua <ngjiahua97@gmail.com>
2024-09-20 11:34:18 +0800

    fix: gpu_taint key check

--- dctwin/interfaces/managers/cfd_manager.py
+++ dctwin/interfaces/managers/cfd_manager.py
@@ -144,7 +144,7 @@ class CFDManager:
                 self.k8s_config["k8s_resources"] = self.k8s_config[
                     "k8s_solving_resources"
                 ]
-                if self.k8s_config["k8s_gpu_taint"]:
+                if "k8s_gpu_taint" in self.k8s_config:
                     self.k8s_config["k8s_taint"] = self.k8s_config["k8s_gpu_taint"]
                 self.solver_backend = SteadySolverBackendK8s(
                     process_num=self.solve_process,
