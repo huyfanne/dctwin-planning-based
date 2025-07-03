76cfcc2531973ce2cf7a5802593b4fdf70b812f6
Ng Jia Hua <ngjiahua97@gmail.com>
2025-07-03 10:18:31 +0800

    fix(zone-builder): defaults ZoneHVAC:EquipmentList to UniformLoad

--- dctwin/third_parties/eplus/builder/zone_builder.py
+++ dctwin/third_parties/eplus/builder/zone_builder.py
@@ -535,7 +535,7 @@ class RoomBuilder:
         zone_name: str,
         acus: Dict[str, ACU],
         dehumidifiers: Dict[str, Dehumidifier],
-        meta: OrderedDict[str, str],
+        meta: RoomMeta,
     ) -> None:
         """
         Build the ZoneHVAC equipment for a thermal zone
@@ -571,6 +571,7 @@ class RoomBuilder:
                 Name=f"{zone_name} returns",
             )
 
+            zone_exhausts = {}
             if num_dehumidifier != 0:
                 zone_exhausts = model.newidfobject(
                     key="NodeList".upper(),
@@ -716,7 +717,7 @@ class RoomBuilder:
         zone_equipment_list = self.model.newidfobject(
             key="ZoneHVAC:EquipmentList".upper(),
             Name=f"{zone_name} equipment list",
-            Load_Distribution_Scheme=meta.load_distribution_scheme,
+            Load_Distribution_Scheme=meta.load_distribution_scheme or "UniformLoad",
         )
         # step 2.1 make air distribution units
         make_air_distribution_units(
